034cd40763c6ac48539d0fda283324f61635d780
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2023-09-20 15:53:52 +0200

    Use rounding, fix acceleration argument

--- R/compute_estimates.R
+++ R/compute_estimates.R
@@ -1,4 +1,5 @@
 compute_estimates <- function(
+		t_icu_discharge = NULL,
 		sampling_frequency = 14L,
 		acceleration_hrqol = 0.0,
 		start_hrqol_arm = 0.1,
@@ -8,11 +9,14 @@ compute_estimates <- function(
 		t_death = Inf,
 		is_mortality_benefitter = FALSE,
 		mortality_trajectory = "exp_decay",
-		mortality_dampening = 0.0
+		mortality_dampening = 0.0,
+
+		n_digits = 3
 ) {
-	patient_traj <- construct_patient_trajectory(
+	patient_trajs <- construct_patient_trajectory(
+		t_icu_discharge = t_icu_discharge,
 		sampling_frequency = sampling_frequency,
-		acceleration_hrqol = start_hrqol_arm,
+		acceleration_hrqol = acceleration_hrqol,
 		start_hrqol_arm = start_hrqol_arm,
 		start_hrqol_patient = start_hrqol_patient,
 		final_hrqol_arm = final_hrqol_arm,
@@ -20,7 +24,9 @@ compute_estimates <- function(
 		t_death = t_death,
 		is_mortality_benefitter = is_mortality_benefitter,
 		mortality_trajectory = mortality_trajectory,
-		mortality_dampening = mortality_dampening
+		mortality_dampening = mortality_dampening,
+
+		n_digits = n_digits
 	)
 
 	purrr::map_dfr(
