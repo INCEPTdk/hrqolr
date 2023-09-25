3b2fd36126ec5da6c56c59607acaf42c5505e930
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2023-09-25 09:32:40 +0200

    Improve code and logic

--- src/bootstrap_mean_diffs.cpp
+++ src/bootstrap_mean_diffs.cpp
@@ -18,8 +18,8 @@
 //' @export
 // [[Rcpp::export]]
 Rcpp::NumericVector bootstrap_mean_diffs (
-	Rcpp::NumericVector vals,
-	Rcpp::IntegerVector grps,
+	const Rcpp::NumericVector& vals,
+	const Rcpp::IntegerVector& grps,
 	int B = 2000,
 	int fixed_seed = 4131
 ) {
@@ -29,24 +29,25 @@ Rcpp::NumericVector bootstrap_mean_diffs (
 
 	// Random engine
 	// std::random_device rd;  // a seed source for the random number engine
-	std::mt19937 gen(fixed_seed); // mersenne_twister_engine seeded with rd()
-	std::uniform_int_distribution<> distrib(0, N-1);
+	std::mt19937 gen(fixed_seed); // mersenne_twister_engine; could also use rd() as seed
+	std::uniform_int_distribution<int> sample_int(0, N-1);
 
 	// Create bootstrap samples
-	std::vector<double> boot_results(B);
+	std::vector<double> boot_results;
+	boot_results.reserve(B);
 
 	for (int b = 0; b < B; b++) {
 		double sums[2] = {0.0, 0.0}; // summed values in actv and ctrl, resp.
 		int ns[2] = {0, 0}; // no. of values in actv and ctrl, resp.
 
 		for (int i = 0; i < N; i++) {
-			int idx = distrib(gen);
+			int idx = sample_int(gen);
 			sums[grps[idx]] += vals[idx];
 			ns[grps[idx]] += 1;
 		}
 
-		boot_results[b] = sums[0]/ns[0] - sums[1]/ns[1];
+		boot_results.push_back(sums[0]/ns[0] - sums[1]/ns[1]);
 	}
 
-	return Rcpp::wrap(boot_results); // Rcpp::wrap() converts std::vector to appropriate Rcpp return type
+	return Rcpp::wrap(boot_results); // Rcpp::wrap() converts to appropriate Rcpp return type
 }
