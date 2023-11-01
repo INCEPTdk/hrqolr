79f6dd60c650b84f93a927a9026a8125df2667e6
epiben <epiben@hey.com>
2023-11-01 10:47:51 +0100

    Make R interface for C_Bootstrap_mean_diffs and add test

--- R/bootstrap_estimates.R
+++ R/bootstrap_estimates.R
@@ -2,36 +2,53 @@
 #'
 #' @inheritParams welch_t_test
 #' @param n_samples scalar, number of bootstrap samples
+#' @param seed int, seed for pseudo-random number generation
 #'
-#' @keywords internal
+#' @export
 #' @return A four-element vector with point estimate, p value and confidence internal
 #'
-bootstrap_estimates <- function (vals, grps, na_replacement = NULL, n_samples = 2000, alpha = 0.05) {
+bootstrap_estimates <- function (
+		vals,
+		grps,
+		reference_arm = head(sort(unique(grps)), 1),
+		na_replacement = NULL,
+		n_samples = 2000,
+		alpha = 0.05,
+		seed = 42
+) {
 	if (!is.null(na_replacement)) {
-		vals <- replace_na(vals, na_replacement)
+		vals[is.na(vals)] <- na_replacement
 	} else {
 		na_idx <- is.na(vals)
 		vals <- vals[!na_idx]
 		grps <- grps[!na_idx]
 	}
 
-	boot_samples <- bootstrap_mean_diffs(vals, grps == "ctrl", B = n_samples)
-		# must convert grps to integer vector
+	vals <- as.double(vals)
 
-	# This is one R-only version (some 12-18 times slower, and .Internal() makes R CMD check fail...):
+	boot_samples <- .Call(
+		"C_Bootstrap_mean_diffs",
+		vals,
+		grps == reference_arm, # must be integer input
+		n_samples,
+		seed
+	)
+	boot_samples <- boot_samples[!is.na(boot_samples)]
+		# happens in the unlikely event that all values are taken from the same arm
+		# in which case the mean difference is undefined
+
+	# This is one R-only version (some 12-18 times slower):
 	# bootstrap_fun <- function(i, idx) {
 	# 	boot_idx <- sample(idx, replace = TRUE)
-	# 	actv_idx <- grps[boot_idx] == "actv"
-	# 	.Internal(mean(vals[boot_idx][actv_idx])) -
-	# 		.Internal(mean(vals[boot_idx][!actv_idx]))
+	# 	ref_idx <- grps[boot_idx] == reference_arm
+	# 	fast_mean(vals[boot_idx][!ref_idx]) - fast_mean(mean(vals[boot_idx][ref_idx]))
 	# }
 	# boot_samples <- sapply(seq_len(n_samples), bootstrap_fun, idx = seq_along(vals))
 
-	est <- .Call("C_Mean", vals[grps == "actv"], PACKAGE = "hrqolr") -
-		.Call("C_Mean", vals[grps == "ctrl"], PACKAGE = "hrqolr")
-	wald_statistic <- (.Call("C_Mean", boot_samples, PACKAGE = "hrqolr") - est)^2 / stats::var(boot_samples)
+	est <- fast_mean(vals[grps != reference_arm]) - fast_mean(vals[grps == reference_arm])
+	wald_statistic <- (fast_mean(boot_samples) - est)^2 / stats::var(boot_samples)
 	p_value <- 1 - stats::pchisq(wald_statistic, df = 1)
 	conf_int <- stats::quantile(boot_samples, c(alpha/2, 1 - alpha/2))
 
-	setNames(c(est, conf_int, p_value), c("point_est", "ci_lo", "ci_hi", "p_values"))
+	setNames(c(est, conf_int, p_value), c("point_est", "ci_lo", "ci_hi", "p_value"))
 }
--- tests/testthat/test-c_functions.R
+++ tests/testthat/test-c_functions.R
@@ -19,3 +19,18 @@ test_that("C_Mean works", {
 	expect_true(is.na(fast_mean(c(as.double(1:10), NA))))
 	expect_error(fast_mean(LETTERS))
 })
+
+test_that("C_Bootstrap_mean_diffs works", {
+	x <- c(
+		0.8, -0.83, 0.49, 1.06, 0.99, 0.27, 2.66, 2.09, -0.53, -0.94,  0.27, 0.61,
+		-0.53, 1.77, 0.47, -0.68, -0.59, -1.01, -0.24, 0.15,  -2.55, 0.18, -1.51, 0,
+		-1.63, -1.77, -0.12, -0.34, -0.73, -0.96,  0.06, -0.56, 0.88, 0.61, -1.17,
+		1.49, -1.49, -1.43, -0.57, -1.86
+	)
+	g <- rep(0:1, length(x) / 2)
+	expect_equal(
+		bootstrap_estimates(x, g, seed = 42),
+		c(point_est = 0.1445, ci_lo = -0.588141571969697, ci_hi = 0.835524025420443,  p_value = 0.999022028057891)
+	)
+})
+
