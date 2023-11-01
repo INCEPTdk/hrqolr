a465bb43e7bf3b1125d9ad368c80d75a5d3791b3
epiben <epiben@hey.com>
2023-11-01 10:48:13 +0100

    Simplify logic (no real performance gain)

--- src/bootstrap_mean_diffs.c
+++ src/bootstrap_mean_diffs.c
@@ -6,35 +6,34 @@ SEXP C_Bootstrap_mean_diffs(SEXP vals, SEXP grps, SEXP B, SEXP seed)
 	// Mimic drand48
 	#define srand48(seed) srand(seed)
 	#define drand48() (rand()/(RAND_MAX + 1.0))
-	srand48((unsigned) asInteger(seed));
+
+	srand48((unsigned) asInteger(seed)); // set the seed
 
 	int n = length(vals);
 	double *pvals = REAL(vals);
 	int *pgrps = INTEGER(grps);
 
-	SEXP boot_results = PROTECT(allocVector(REALSXP, asInteger(B)));
-	double *pboot_results = REAL(boot_results);
+	SEXP boot_estimates = PROTECT(allocVector(REALSXP, asInteger(B)));
+	double *pboot_estimates = REAL(boot_estimates);
 
-	SEXP sums = PROTECT(allocVector(REALSXP, 2));
-	double *psums = REAL(sums);
-
-	SEXP ns = PROTECT(allocVector(INTSXP, 2));
-	int *pns = INTEGER(ns);
+	double sums[2] = {0.0, 0.0}; // summed values in actv and ctrl, resp.
+	int ns[2] = {0, 0}; // no. of values in actv and ctrl, resp.
+	int idx = -1; // set to invalid index just in case
 
 	for (int b = 0; b < asInteger(B); b++) {
-		psums[0] = 0.0; psums[1] = 0.0; // summed values in intv and ctrl, resp.
-		pns[0] = 0; pns[1] = 0; // no. of values in intv and ctrl, resp.
+		sums[0] = 0.0; sums[1] = 0.0;
+		ns[0] = 0; ns[1] = 0;
 
 		for (int i = 0; i < n; i++) {
-			int idx = n * drand48();
-			psums[pgrps[idx]] += pvals[idx];
-			pns[pgrps[idx]] += 1.0;
+			idx = n * drand48();
+			sums[pgrps[idx]] += pvals[idx];
+			ns[pgrps[idx]] += 1.0;
 		}
 
-		pboot_results[b] = psums[0]/pns[0] - psums[1]/pns[1];
+		pboot_estimates[b] = sums[0]/ns[0] - sums[1]/ns[1];
 	}
 
-	UNPROTECT(3);
+	UNPROTECT(1);
 
-	return boot_results;
+	return boot_estimates;
 }
