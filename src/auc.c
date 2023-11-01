c795772fbeb6add5b877d899f569406c89317929
epiben <epiben@hey.com>
2023-11-01 14:26:32 +0100

    Improve initialisation in C_Auc

--- src/auc.c
+++ src/auc.c
@@ -6,11 +6,15 @@ SEXP C_Auc(SEXP x, SEXP y)
 	int n = length(x);
 	double *px = REAL(x);
 	double *py = REAL(y);
-	double auc = 0.0;
+	SEXP auc = PROTECT(allocVector(REALSXP, 1));
+	double *pauc = REAL(auc);
+	memset(pauc, 0.0, sizeof(double));
 
 	for (int i = 1; i < n; i++) {
-		auc += (px[i] - px[i-1]) * (py[i] + py[i-1]) * 0.5;
+		pauc[0] += (px[i] - px[i-1]) * (py[i] + py[i-1]) * 0.5;
 	}
 
-	return ScalarReal(auc);
+	UNPROTECT(1);
+
+	return auc;
 }
