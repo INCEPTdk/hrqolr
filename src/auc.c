94e74008fc318ace8dde53d8729e8c031a539dd4
epiben <epiben@hey.com>
2023-11-01 14:56:06 +0100

    Use R_alloc intead of PROTECT()

--- src/auc.c
+++ src/auc.c
@@ -6,15 +6,12 @@ SEXP C_Auc(SEXP x, SEXP y)
 	int n = length(x);
 	double *px = REAL(x);
 	double *py = REAL(y);
-	SEXP auc = PROTECT(allocVector(REALSXP, 1));
-	double *pauc = REAL(auc);
-	memset(pauc, 0.0, sizeof(double));
+	double *auc = (double *) R_alloc(1, sizeof(double));
+	*auc = 0.0;
 
 	for (int i = 1; i < n; i++) {
-		pauc[0] += (px[i] - px[i-1]) * (py[i] + py[i-1]) * 0.5;
+		*auc += (px[i] - px[i-1]) * (py[i] + py[i-1]) * 0.5;
 	}
 
-	UNPROTECT(1);
-
-	return auc;
+	return ScalarReal(*auc);
 }
--- src/mean.c
+++ src/mean.c
@@ -6,13 +6,11 @@ SEXP C_Mean(SEXP x)
 	int n = length(x);
 	double *px = REAL(x);
 
-	SEXP sum = PROTECT(allocVector(REALSXP, 1));
-	double *psum = REAL(sum);
-	psum[0] = 0.0;
+	double *sum = (double *) R_alloc(1, sizeof(double));
+	*sum = 0.0;
 	for (int i = 0; i < n; i++) {
-		psum[0] += px[i];
+		*sum += px[i];
 	}
-	UNPROTECT(1);
 
-	return ScalarReal(asReal(sum) / length(x));
+	return ScalarReal(*sum / n);
 }
