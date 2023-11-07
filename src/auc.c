eb8e9a226c2b9364b2c0bdbdafb745fa998da1c5
epiben <epiben@hey.com>
2023-11-07 16:00:22 +0100

    Don't use R_alloc

--- src/auc.c
+++ src/auc.c
@@ -8,12 +8,11 @@ SEXP C_Auc(SEXP x, SEXP y)
 	px = REAL(x);
 	py = REAL(y);
 
-	double *auc = (double *) R_alloc(1, sizeof(double));
-	*auc = 0.0;
+	double auc = 0.0;
 
 	for (int i = 1; i < n; i++) {
-		*auc += (px[i] - px[i-1]) * (py[i] + py[i-1]) * 0.5;
+		auc += (px[i] - px[i-1]) * (py[i] + py[i-1]) * 0.5;
 	}
 
-	return ScalarReal(*auc);
+	return ScalarReal(auc);
 }
--- src/mean.c
+++ src/mean.c
@@ -7,11 +7,10 @@ SEXP C_Mean(SEXP x)
 	double *px;
 	px = REAL(x);
 
-	double *sum = (double *) R_alloc(1, sizeof(double));
-	*sum = 0.0;
+	double sum = 0.0;
 	for (int i = 0; i < n; i++) {
-		*sum += px[i];
+		sum += px[i];
 	}
 
-	return ScalarReal(*sum / n);
+	return ScalarReal(sum / n);
 }
