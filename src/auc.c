49242759b6a881748dee70c6b9801fa647f26038
epiben <epiben@hey.com>
2023-11-09 10:03:54 +0100

    As R_alloc anew

--- src/auc.c
+++ src/auc.c
@@ -2,24 +2,19 @@
 #include <Rinternals.h>
 #include "hrqolr.h"
 
-SEXP C_Auc(SEXP x, SEXP y)
+SEXP auc(SEXP x, SEXP y)
 {
 	size_t n = length(x);
-
 	double *px, *py;
 	px = REAL(x);
 	py = REAL(y);
 
-	SEXP auc = PROTECT(allocVector(REALSXP, 1));
-	double *_auc;
-	_auc = REAL(auc);
-	_auc[0] = 0.0;
+	double *auc = (double *) R_alloc(1, sizeof(double));
+	auc[0] = 0.0;
 
 	for (int i = 1; i < n; i++) {
-		_auc[0] += (px[i] - px[i-1]) * (py[i] + py[i-1]) * 0.5;
+		auc[0] += (px[i] - px[i-1]) * (py[i] + py[i-1]) * 0.5;
 	}
 
-	UNPROTECT(1);
-
-	return auc;
+	return ScalarReal(*auc);
 }
--- src/mean.c
+++ src/mean.c
@@ -9,18 +9,14 @@ SEXP mean(SEXP x)
 	double *px;
 	px = REAL(x);
 
-	SEXP res = PROTECT(allocVector(REALSXP, 1));
-	double *_res;
-	_res = REAL(res);
-	_res[0] = 0.0;
+	double *res = (double *) R_alloc(1, sizeof(double));
+	res[0] = 0.0;
 
 	for (int i = 0; i < n; i++) {
-		_res[0] += px[i];
+		res[0] += px[i];
 	}
 
-	_res[0] /= n;
+	res[0] /= n;
 
-	UNPROTECT(1);
-
-	return res;
+	return ScalarReal(*res);
 }
