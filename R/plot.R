7a8ebce3f92f2e7cd79db35fb9674a118b7a556c
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2024-06-04 09:51:25 +0200

    another attempt at fixing plot.simulate_trial so tests also pass in the github action

--- R/plot.R
+++ R/plot.R
@@ -91,12 +91,13 @@ plot.hrqolr_trajectories <- function(
 #'
 #' @param analysis character, `"all"` (include non-survivors and set their HRQoL to 0) or `"survivors"`.
 #' @param ecdf logical, whether to plot the empiricial cumulative distribution (default) or not
+#' @inheritParams stats::density
 #'
 #' @import data.table
 #' @export
 #' @describeIn plot Single trial
 #'
-plot.hrqolr_trial <- function(x, analysis = "all", ecdf = TRUE, ...) {
+plot.hrqolr_trial <- function(x, analysis = "all", ecdf = TRUE, n = 512, ...) {
 	assert_pkgs("ggplot2")
 	x <- copy(x$patient_results) # don't corrupt input object
 
@@ -109,22 +110,35 @@ plot.hrqolr_trial <- function(x, analysis = "all", ecdf = TRUE, ...) {
 
 	dt <- melt.data.table(x, id.vars = id_cols, measure.vars = outcome_cols)
 
-	p_base <- ggplot2::ggplot(dt, ggplot2::aes(x = value, colour = arm)) +
+	compute_y <- function(x, ecdf, n) {
+		if (isTRUE(ecdf)) {
+			x_grid <- seq(min(x), max(x), length.out = n)
+			list(x = x_grid, y = stats::ecdf(x)(x_grid))
+		} else {
+			stats::density(x, n = n)[c("x", "y")]
+		}
+	}
+
+	dt <- dt[, compute_y(x = value, ecdf = ecdf, n = n), by = c("arm", "variable")]
+
+	p_base <- ggplot2::ggplot(dt, ggplot2::aes(x = x, y = y, colour = arm)) +
+		ggplot2::geom_line(na.rm = TRUE) +
 		ggplot2::facet_wrap(~ variable, scales = "free") +
-		ggplot2::theme(legend.title = ggplot2::element_blank())
+		ggplot2::theme(
+			legend.title = ggplot2::element_blank(),
+			axis.title.x = ggplot2::element_blank()
+		)
 
 	if (isTRUE(ecdf)) {
-		p_final <- p_base +
-			ggplot2::stat_ecdf(na.rm = TRUE, pad = FALSE) +
-			ggplot2::scale_y_continuous(labels = scales::percent)
+		p_base +
+			ggplot2::scale_y_continuous(labels = scales::percent) +
+			ggplot2::labs(y = "Cumulative density")
 	} else {
-		p_final <- p_base +
-			ggplot2::stat_density(geom = "line", position = "identity", na.rm = TRUE, trim = TRUE) +
+		p_base +
 			ggplot2::theme(
 				axis.text.y = ggplot2::element_blank(),
 				axis.ticks.y = ggplot2::element_blank()
-			)
+			) +
+			ggplot2::labs(y = "Density")
 	}
-
-	return(p_final)
 }
